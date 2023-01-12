baeba981f3d9b0a124b6875dee40480cd96b4a86
Srinivas <srinivasa.reddy@cognerium.com>
2023-01-12 15:30:35 +0530

    animatedcroassfade

--- lib/main.dart
+++ lib/main.dart
@@ -21,43 +21,29 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   // Here we are creating the bool to use animation
-  bool isupdate=false;
+  bool _first=false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
 
       body: Center(
-        // Here we are using the Animated Container so we can animate any property of container
-        // such as height, width, alignment, gradient,radius,color etc
-        child: AnimatedContainer(
+        /*
+        This code fades between two representations of the Flutter widget. It depends on a boolean field _first; when _first is true, the first widget is shown, otherwise the second widget is shown. When the field changes state, the AnimatedCrossFade widget cross-fades between the two forms of the widget over three seconds.
+        * */
+        child: AnimatedCrossFade(
 
-        margin:const  EdgeInsets.all(30),
-        height: isupdate ? 200:400,
-      width:isupdate?200: 400,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(isupdate?50:5git ),
-        gradient: LinearGradient(
-          colors:isupdate?[Colors.pinkAccent,Colors.purple]:
-           [Colors.blue,Colors.tealAccent]
-        )
-      ),
-
-        duration:const  Duration(seconds: 2),
-        alignment:isupdate?Alignment.center: Alignment.topRight,
-        child:const CircleAvatar(
-          backgroundColor: Colors.white,
-        )
-
-
-
-      ),),
+          duration: const Duration(seconds: 3),
+          firstChild: Container(height: 300,width: 300,decoration:const  BoxDecoration(image: DecorationImage(image: AssetImage("assets/image.jpeg"),fit: BoxFit.fill)),),
+          secondChild:Container(height: 300,width: 300,decoration :const BoxDecoration(image: DecorationImage(image: AssetImage("assets/image2.jpeg"),fit: BoxFit.fill)),),
+          crossFadeState: _first ? CrossFadeState.showFirst : CrossFadeState.showSecond,
+        )),
       floatingActionButton: FloatingActionButton(onPressed: () {
         // here we are changing the state as well as the value
         setState(() {
-          if(isupdate){
-            isupdate=false;
+          if(_first){
+            _first=false;
           }else{
-            isupdate=true;
+            _first=true;
 
           }
 
