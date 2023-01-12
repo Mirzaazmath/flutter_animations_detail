ee3b361bac0674d48c62dfcdb6f4b5d44037cc67
Srinivas <srinivasa.reddy@cognerium.com>
2023-01-12 15:41:23 +0530

    animatedDefaultTextStyle

--- lib/main.dart
+++ lib/main.dart
@@ -21,29 +21,29 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   // Here we are creating the bool to use animation
-  bool _first=false;
+  bool _update=false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
 
       body: Center(
         /*
-        This code fades between two representations of the Flutter widget. It depends on a boolean field _first; when _first is true, the first widget is shown, otherwise the second widget is shown. When the field changes state, the AnimatedCrossFade widget cross-fades between the two forms of the widget over three seconds.
-        * */
-        child: AnimatedCrossFade(
 
-          duration: const Duration(seconds: 3),
-          firstChild: Container(height: 300,width: 300,decoration:const  BoxDecoration(image: DecorationImage(image: AssetImage("assets/image.jpeg"),fit: BoxFit.fill)),),
-          secondChild:Container(height: 300,width: 300,decoration :const BoxDecoration(image: DecorationImage(image: AssetImage("assets/image2.jpeg"),fit: BoxFit.fill)),),
-          crossFadeState: _first ? CrossFadeState.showFirst : CrossFadeState.showSecond,
-        )),
+        * */
+        child:AnimatedDefaultTextStyle(
+          style: TextStyle(
+            fontSize:_update?30: 100,
+              fontWeight:_update?FontWeight.w300: FontWeight.bold,
+              color:_update?Colors.red :Colors.blue),
+        duration: Duration(seconds: 2),
+        child: Text("Flutter "),) ),
       floatingActionButton: FloatingActionButton(onPressed: () {
         // here we are changing the state as well as the value
         setState(() {
-          if(_first){
-            _first=false;
+          if(_update){
+            _update=false;
           }else{
-            _first=true;
+            _update=true;
 
           }
 
